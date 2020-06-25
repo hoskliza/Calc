f8216e4b40055de1903fe94b98f5e39d12a62c4c
Liza <lizafranken32@gmail.com>
2020-06-25 19:38:53 +0300

    Update

---
+++ Calc/MathOperationsLab2.cs
@@ -0,0 +1,15 @@
+namespace Calc
+{
+    public class MathOperationsLab2
+    {
+        public static float Sum(float a, float b, float c)
+        {
+            return a + b + c;
+        }
+
+        public static float Multiply(float a, float b, float c)
+        {
+            return a * b * c;
+        }
+    }
+}
---
+++ Calc/MathOperationsLab3.cs
@@ -0,0 +1,15 @@
+namespace Calc
+{
+    public class MathOperationsLab3
+    {
+        public static float Sum(float a, float b, float c, float d)
+        {
+            return a + b + c + d;
+        }
+
+        public static float Multiply(float a, float b, float c, float d)
+        {
+            return a * b * c * d;
+        }
+    }
+}
